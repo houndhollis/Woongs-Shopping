aa6e5945b31e996b006ef9a22a4d26c14624255a
houndhollis <houndhollis9797@gmail.com>
2022-11-04 00:17:54 +0900

    feat:graphql 초기세팅

---
+++ src/graphql/products.ts
@@ -0,0 +1,23 @@
+import { gql } from "graphql-tag";
+
+export type PRODUCT = {
+  id:string
+  imageUrl:string
+  price:number
+  title:string
+  description:string
+  createAt:string
+}
+
+const GET_PRODUCTS = gql`
+  query GET_PRODUCTS {
+    id
+    imageUrl
+    price
+    title
+    description
+    createAt
+  }
+`
+
+export default GET_PRODUCTS
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
-import './scss/index.scss'
-import App from './app'
+import './scss/index.scss';
+import App from './app';
+import { worker } from './mocks/browser'
+
+if (import.meta.env.DEV) {
+  worker.start()
+}
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
